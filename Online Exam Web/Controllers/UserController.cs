4d78bfa1facc65ff11ea2b17dbe5149c052e5e11
Digvijay singh <dssodha1234@gmail.com>
2023-01-13 12:59:46 +0530

    admin user result view

--- Online Exam Web/Controllers/UserController.cs
+++ Online Exam Web/Controllers/UserController.cs
@@ -4,58 +4,99 @@ using Microsoft.AspNetCore.Mvc;
 using OnlineExam.DataAccess.data;
 using OnlineExam.DataAccess.Repository.IRepository;
 using OnlineExam.Models;
+using OnlineExam.Models.ViewModels;
 
 namespace Online_Exam_Web.Controllers;
-    [Authorize(Roles = "1")]
-    public class UserController : Controller
-    {
-        public readonly IUnitOfWork unitOfWork;
+[Authorize(Roles = "1")]
+public class UserController : Controller
+{
+	public readonly IUnitOfWork unitOfWork;
 
-        public UserController(IUnitOfWork nitOfWork)
-        {
-            unitOfWork = nitOfWork;
-        }
+	public UserController(IUnitOfWork nitOfWork)
+	{
+		unitOfWork = nitOfWork;
+	}
 
-        public IActionResult Index()
-        {
-            var objList = unitOfWork.UserRepo.GetAll();
+	public IActionResult Index()
+	{
+		var objList = unitOfWork.UserRepo.GetAll();
 
-            return View(objList);
-        }
+		return View(objList);
+	}
 
-        //get
-        public IActionResult Edit(int? id)
-        {
-            if (id == 0 || id is null)
-            {
-                return NotFound();
-            }
-            var user = unitOfWork.UserRepo.GetFirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
+	//get
+	public IActionResult Edit(int? id)
+	{
+		if (id == 0 || id is null)
+		{
+			return NotFound();
+		}
+		var user = unitOfWork.UserRepo.GetFirstOrDefault(u => u.Id == id);
+		if (user == null)
+		{
+			return NotFound();
+		}
+		return View(user);
+	}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(User obj)
-        {
-            var user = unitOfWork.UserRepo.GetFirstOrDefault(u => u.Id == obj.Id);
-            user.IsActive = obj.IsActive;
-            unitOfWork.UserRepo.Update(user);
-            unitOfWork.Save();
-            return RedirectToAction("Index");
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	public IActionResult Edit(User obj)
+	{
+		var user = unitOfWork.UserRepo.GetFirstOrDefault(u => u.Id == obj.Id);
+		user.IsActive = obj.IsActive;
+		unitOfWork.UserRepo.Update(user);
+		unitOfWork.Save();
+		return RedirectToAction("Index");
 
-            return View(obj);
-        }
+		return View(obj);
+	}
 
-        public IActionResult UserExamDetails()
-        {
-			//make subject appear which are attempted or one
-			var objList = unitOfWork.UserSub.GetAll("Subject").Where(e => e.UserId == Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier)) && e.Counter != 0);
-			return View(objList.ToList());
+	public IActionResult UserExamDetails(int? id)
+	{
+		//make subject appear which are attempted or one
+		var objList = unitOfWork.UserSub.GetAll("Subject").Where(e => e.UserId == id && e.Counter != 0);
+		return View(objList.ToList());
+	}
+	#region Api calls
+	[HttpGet]
+	public IActionResult GetExamResultByAdmin(int? id, int uId = 0)
+	{
+		/*id = 10;*/
+		if (id == 0 || id is null)
+		{
+			return null;
+		}
+		if (uId == 0)
+		{
+			//triggers when user himself loged in
+			uId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
 		}
 
-    }
+
+		List<ExamVM> exam = new List<ExamVM>();
+		var result = unitOfWork.ResultRepo.GetAll().Where(e => e.SubjectId == id && e.UserId == uId);
+		/*so initially my result Model's column with name wasCurrect was a bool type and it stored
+		 * bit type(0,1) of values so when itried to fetch data from data base it throw exception 
+		 * as:--System.InvalidCastException: 'Unable to cast object of type 'System.String' to type 'System.Boolean'.'
+		 * so my question is that previousely i have used few models and in those cases i have a 
+		 * column called IsActive and it is bool type but in those cases i never got this error when
+		 * I was doing crud on them so why did i got this error particularly for result model
+		 */
+		result = result.ToList();
+		foreach (var item in result)
+		{
+			var questionSet = new ExamVM
+			{
+				result = item,
+				OptionsList = unitOfWork.OptionRepo.GetAll("").Where(e => e.QuestionId == item.QuestionId).ToList(),
+				question = unitOfWork.QuesRepo.GetFirstOrDefault(e => e.Id == item.QuestionId),
+			};
+			exam.Add(questionSet);
+		}
+		/*return RedirectToAction("AttemptWIthApi");*/
+		/*var a = Json(new { examdata = exam });*/
+		return Json(new { examdata = exam });
+	}
+	#endregion
+}
---
+++ OnlineExam.DataAccess/Migrations/20230112055536_resultModeWasCurrectColumnchanged.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineExamWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class resultModeWasCurrectColumnchanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "wasCurrect",
+                table: "Results",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "wasCurrect",
+                table: "Results",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
